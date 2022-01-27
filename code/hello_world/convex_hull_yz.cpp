7d22220dbbb145acb198c156d3c5fc9fbcb009ea
geo-j <g.juglan@tutanota.com>
2022-01-27 13:24:25 +0100

    add hello world convex hull yz

---
+++ code/hello_world/convex_hull_yz.cpp
@@ -0,0 +1,20 @@
+// for when we want input for third-party libraries
+
+#include <iostream>
+#include <iterator>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Projection_traits_yz_3.h>
+#include <CGAL/convex_hull_2.h>
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K3;
+typedef CGAL::Projection_traits_yz_3<K3> K;
+typedef K::Point_2 Point_2;
+int main()
+{
+  std::istream_iterator< Point_2 >  input_begin( std::cin );
+  std::istream_iterator< Point_2 >  input_end;
+  std::ostream_iterator< Point_2 >  output( std::cout, "dingus\n" );
+  CGAL::convex_hull_2( input_begin, input_end, output, K() );
+//   std::cout << output << " points on the convex hull" << std::endl;
+
+  return 0;
+}
