2c128f22de746db96ff31377695e6f4e1818cabf
geo-j <geo.potato@tutanota.com>
2022-02-02 15:08:29 +0100

    started added initialisings

--- code/hello_world/joe_simpson_visibility.cpp
+++ code/hello_world/joe_simpson_visibility.cpp
@@ -5,6 +5,7 @@
 #include <CGAL/Object.h>
 #include <CGAL/Polygon_2.h>
 #include <CGAL/Arr_naive_point_location.h>
+#include <CGAL/Arr_walk_along_line_point_location.h>
 
 #include <stack>
 
@@ -20,8 +21,16 @@ typedef CGAL::Polygon_2<Kernel>                                         Polygon_
 typedef typename Geometry_traits_2::Ray_2                               Ray_2;
 typedef typename Geometry_traits_2::Object_2                            Object_2;
 typedef typename Kernel::Intersect_2                                    Intersect_2;
-
-
+typedef Arrangement_2::Edge_const_iterator                              Edge_const_iterator;
+typedef Arrangement_2::Ccb_halfedge_circulator                          Ccb_halfedge_circulator;
+typedef typename Arrangement_2::Ccb_halfedge_const_circulator           Ccb_halfedge_const_circulator;
+typedef typename Arrangement_2::Face_const_handle                       Face_const_handle;
+typedef CGAL::Arr_walk_along_line_point_location<Arrangement_2>         Arr_point_location;
+typedef typename Arr_point_location::result_type                        Location_result;
+typedef typename Arrangement_2::Halfedge_const_handle                   Halfedge_const_handle;
+typedef typename Geometry_traits_2::Line_2                              Line_2;
+typedef typename Arrangement_2::Vertex_const_handle                     Vertex_const_handle;
+typedef typename Arrangement_2::Halfedge_around_vertex_const_circulator Halfedge_around_vertex_const_circulator;
 
 typedef std::vector<Point_2>                                            Vertex_container;
 typedef typename Vertex_container::size_type                            Size_type;
@@ -30,6 +39,7 @@ typedef typename Vertex_container::size_type                            Size_typ
 Vertex_container vertices;
 std::stack<Point_2> S;
 Geometry_traits_2 *traits;
+Arr_point_location point_location;
 
 
 enum {LEFT, RIGHT, SCANA, SCANB, SCANC, SCAND, FINISH} oper;
@@ -278,7 +288,7 @@ void output(const Point_2& p, Arrangement_2& out_arr) {
     v_last = v_first = out_arr.insert_in_face_interior(points[0], out_arr.unbounded_face());
 	
     for(unsigned int i = 0; i < points.size() - 1; i ++){
-        if(points[i] < points[(i+1)]){
+        if(points[i] < points[i + 1]){
             v_last = out_arr.insert_from_left_vertex (Segment_2(points[i], points[i + 1]), v_last)->target();
         } else {
             v_last = out_arr.insert_from_right_vertex(Segment_2(points[i], points[i + 1]), v_last)->target();
@@ -287,84 +297,120 @@ void output(const Point_2& p, Arrangement_2& out_arr) {
     out_arr.insert_at_vertices(Segment_2(points.front(), points.back()), v_last, v_first);
 }
 
+  /*! Finds a visible vertex from the query point 'q' in 'face' 
+    to start the algorithm from*/
+Ccb_halfedge_const_circulator find_visible_start(Face_const_handle face, Point_2 &q) {
+    Location_result result = point_location.ray_shoot_up(q);
+
+    if (const Halfedge_const_handle* e = boost::get<Halfedge_const_handle>(&(result))) {
+	    Point_2 p(q.x(), traits->compute_y_at_x_2_object()(Line_2((*e)->source()->point(), (*e)->target()->point()), q.x()));
+
+        vertices.push_back(p);
+        // inserted_artificial_starting_vertex = true;
+
+        return (*e)->next()->ccb();
+    } else if (const Vertex_const_handle* v = boost::get<Vertex_const_handle>(&(result))) {
+	    Halfedge_around_vertex_const_circulator cir = (*v)->incident_halfedges();
+
+	while(face != cir->face()) {
+	  ++cir;
+	}
+	return cir->next()->ccb();
+      }
+    else
+      {
+	CGAL_assertion_msg(false, "Should not be reachable.");
+	return Ccb_halfedge_const_circulator();
+      }
+  }
+
 Arrangement_2 compute_visibility_polygon(Point_2 p) {
     /*
     Orientation_2 operator()(Point_2 p, Point_2 q, Point_2 r) that returns CGAL::LEFT_TURN, if r lies to the left of the oriented line l defined by p and q,
                                                                            CGAL::RIGHT_TURN if r lies to the right of l, and 
                                                                            CGAL::COLLINEAR if r lies on l. 
    */
+    Ccb_halfedge_const_circulator circ = find_visible_start(face, q);
+    Ccb_halfedge_const_circulator curr = circ;
+
+    do {
+      vertices.push_back(curr->source()->point());
+    } while(++curr != circ);
+
+    vertices.push_back(vertices[0]);
+
     CGAL::Orientation orientation = traits->orientation_2_object()(p, vertices[0], vertices[1]);  // check where vertices[1] (v_1) lies w.r.t. the line pvertices[0] (pv_0)
     Point_2 w;
     Size_type i = 0;
     Arrangement_2 out_arr;
 
-    if (orientation != CGAL::RIGHT_TURN) {
-        oper = LEFT;
-        i = 1;
-        S.push(vertices[0]);
-        S.push(vertices[1]);
-    } else {
-        oper = SCANA;
-        i = 1;
-        w = vertices[1];
-        S.push(vertices[0]);
-    }
+    // if (orientation != CGAL::RIGHT_TURN) {
+    //     oper = LEFT;
+    //     i = 1;
+    //     S.push(vertices[0]);
+    //     S.push(vertices[1]);
+    // } else {
+    //     oper = SCANA;
+    //     i = 1;
+    //     w = vertices[1];
+    //     S.push(vertices[0]);
+    // }
 
-    do {
-        switch(oper) {
-            case LEFT:
-                left(i, w, p);
-                break;
-            case RIGHT:
-                right(i, w, p);
-                break;
-            case SCANA:
-                scana(i, w, p);
-                break;
-            case SCANB:
-                scanb(i, w);
-                break;
-            case SCANC:
-                scanc(i, w);
-                break;
-            case SCAND:
-                scand(i, w);
-                break;
-        }
+    // do {
+    //     switch(oper) {
+    //         case LEFT:
+    //             left(i, w, p);
+    //             break;
+    //         case RIGHT:
+    //             right(i, w, p);
+    //             break;
+    //         case SCANA:
+    //             scana(i, w, p);
+    //             break;
+    //         case SCANB:
+    //             scanb(i, w);
+    //             break;
+    //         case SCANC:
+    //             scanc(i, w);
+    //             break;
+    //         case SCAND:
+    //             scand(i, w);
+    //             break;
+    //     }
 
-        Point_2 prev_tos = S.top();
-        S.pop();
+    //     Point_2 prev_tos = S.top();
+    //     S.pop();
 
-        if (oper == LEFT && 
-            // check if s_{t - 1}s_t intersect pv_0
-            traits->orientation_2_object()(p, vertices[0], S.top()) == CGAL::RIGHT_TURN && // check if s_t is on the right of v0
-            traits->orientation_2_object()(p, vertices[0], prev_tos) == CGAL::LEFT_TURN) { // check if s_{t - 1} is on the left of v0
-                Point_2 tos = S.top();
-                S.pop();
+    //     if (oper == LEFT && 
+    //         // check if s_{t - 1}s_t intersect pv_0
+    //         traits->orientation_2_object()(p, vertices[0], S.top()) == CGAL::RIGHT_TURN && // check if s_t is on the right of v0
+    //         traits->orientation_2_object()(p, vertices[0], prev_tos) == CGAL::LEFT_TURN) { // check if s_{t - 1} is on the left of v0
+    //             Point_2 tos = S.top();
+    //             S.pop();
 
-                Segment_2 seg(S.top(), tos);
-                Ray_2 ray_origin(p, vertices[0]);
+    //             Segment_2 seg(S.top(), tos);
+    //             Ray_2 ray_origin(p, vertices[0]);
 
-                if (Object_2 result = Intersect_2()(seg, ray_origin)) {
-                    const Point_2 *intersection_point = CGAL::object_cast<Point_2>(&result);
-                    tos = *intersection_point;
+    //             if (Object_2 result = Intersect_2()(seg, ray_origin)) {
+    //                 const Point_2 *intersection_point = CGAL::object_cast<Point_2>(&result);
+    //                 tos = *intersection_point;
 
-                    oper = SCANB;
-                }
+    //                 oper = SCANB;
+    //             }
 
-            S.push(tos);
-        }
+    //         S.push(tos);
+    //     }
 
-    } while(oper != FINISH);
+    // } while(oper != FINISH);
 
-    output(p, out_arr);
+    // output(p, out_arr);
 
     return out_arr;
 }
 
 int main() {
     Polygon_2 P;
-    Point_2 p1(0,4), p2(0,0), p3(3,2), p4(4,0), p5(4,4), p6(1,2);
+    Point_2 p1(0, 4), p2(0, 0), p3(3, 2), p4(4, 0), p5(4, 4), p6(1, 2);
     P.push_back(p1);
     P.push_back(p2);
     P.push_back(p3);
@@ -384,13 +430,19 @@ int main() {
 
     // find the face of the query point
     // (usually you may know that by other means)
-    Point_2 q(0.5, 2);
+    Point_2 p(0.5, 2);
     Arrangement_2::Face_const_handle * face;
     CGAL::Arr_naive_point_location<Arrangement_2> pl(env);
-    CGAL::Arr_point_location_result<Arrangement_2>::Type obj = pl.locate(q);
+    CGAL::Arr_point_location_result<Arrangement_2>::Type obj = pl.locate(p);
 
     // The query point locates in the interior of a face
     face = boost::get<Arrangement_2::Face_const_handle> (&obj);
+
+    std::cout << "here";
+    Arrangement_2 output = compute_visibility_polygon(p);
+
+    // for (Edge_const_iterator eit = output.edges_begin(); eit != output.edges_end(); ++ eit)
+    //     std::cout << "[" << eit->source()->point() << " -> " << eit->target()->point() << "]" << std::endl;
 
     return 0;
 }
