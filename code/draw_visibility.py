ac7ce9b5ceb9176bc56e156fb88c45d1ef9a3d11
geo-j <g.juglan@tutanota.com>
2022-02-09 10:52:41 +0100

    draw arrangement with thin lines

---
+++ code/draw_visibility.py
@@ -0,0 +1,47 @@
+from skgeom.draw import draw
+from skgeom import Segment2, Point2, arrangement, RotationalSweepVisibility
+import matplotlib.pyplot as plt
+
+
+INPUT = "arrangement_main_out"
+
+
+arr = arrangement.Arrangement()
+
+with open(INPUT, "r") as f:
+    E = int(f.readline())
+
+    for e in range(E):
+        segment = list(map(int, f.readline().split()))
+        p1 = Point2(*segment[:2])
+        p2 = Point2(*segment[2:])
+
+        arr.insert(Segment2(p1, p2))
+
+
+vs = RotationalSweepVisibility(arr)
+
+q = Point2(0.5, 2)
+z = Point2(1, 1.5)
+face1 = arr.find(q)
+face2 = arr.find(z)
+vx1 = vs.compute_visibility(q, face1)
+vx2 = vs.compute_visibility(z, face2)
+
+
+for v in vx1.halfedges:
+    draw(v.curve(), point = q, color='red', visible_point=False, fill = True)
+    # draw(v.curve(), z, color='green', visible_point=False)
+
+for v in vx2.halfedges:
+    draw(v.curve(), point = z, color='blue', visible_point=False, fill = True)
+    # draw(v.curve(), z, color='green', visible_point=False)
+for he in arr.halfedges:
+    draw(he.curve(), visible_point=True)
+
+draw(q,  color='magenta')
+draw(z, color='magenta')
+
+
+plt.show()
+
