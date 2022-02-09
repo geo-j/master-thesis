db13a270971447c9be8adaa39a1c17bfb5b46b3e
geo-j <g.juglan@tutanota.com>
2022-02-09 14:57:43 +0100

    fix full visibility I HOPE

--- code/main.cpp
+++ code/main.cpp
@@ -8,11 +8,17 @@ int main() {
     f >> arrangement;
 
     // TODO: for now guards are manually added/hard-coded. See how it evolves in the future. Maybe only keep guards for output, since it's the algorithm's job to place them.
-    Point_2 p(0.5, 2);
-    arrangement.add_guard(p);
+    Point_2 p1(0.5, 2), p2(3.5, 2.5);
+    arrangement.add_guard(p1);
+    arrangement.add_guard(p2);
 
     g << arrangement;
     arrangement.print_guards(g);
+
+    auto visible_vertices = arrangement.compute_visibility();
+    for (auto v : visible_vertices)
+        std::cout << v << std::endl;
+    std::cout << arrangement.is_completely_visible(visible_vertices) << std::endl;
 
     return 0;
 }
