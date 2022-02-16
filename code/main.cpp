5a8176b60274c115da03f5700a12495bb1746792
geo-j <g.juglan@tutanota.com>
2022-02-16 15:29:52 +0100

    read guards only main

--- code/main.cpp
+++ code/main.cpp
@@ -2,24 +2,18 @@
 
 
 int main() {
-    std::ifstream f("pentagram.in");
-    std::ofstream g("pentagram.out");
     Arrangement arrangement;
-    // TODO: maybe leave cin/cout, and pipe files in?
+    // TODO: piping multiple files in doesn't work. probably a fish problem?
     std::cin >> arrangement;
+    arrangement.read_guards(std::cin);
 
-    // TODO: for now guards are manually added/hard-coded. See how it evolves in the future. Maybe only keep guards for output, since it's the algorithm's job to place them.
-    Point_2 p_crown(4, 5.5), p_pentagram1(12.5, 4.5), p_pentagram2(14, 6), p_triangle1(5.5, 10.5), p_triangle2(2.5, 10.5);
-    arrangement.add_guard(p_pentagram1);
-    arrangement.add_guard(p_pentagram2);
+    // std::cout << arrangement;
+    // arrangement.print_guards(std::cout);
 
-    std::cout << arrangement;
-    arrangement.print_guards(std::cout);
-
-    auto visible_vertices = arrangement.compute_visibility();
+    auto visible_vertices = arrangement.compute_full_visibility();
     // for (auto v : visible_vertices)
         // std::cout << v << std::endl;
-    // std::cout << arrangement.is_completely_visible(visible_vertices) << std::endl;
+    std::cout << arrangement.is_completely_visible(visible_vertices) << std::endl;
 
     return 0;
 }
