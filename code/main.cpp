82fc398ca992c82338f80f39c093080a56936054
geo-j <g.juglan@tutanota.com>
2022-03-18 17:26:13 +0100

    new main who dis

--- code/main.cpp
+++ code/main.cpp
@@ -3,17 +3,17 @@
 
 int main() {
     Arrangement arrangement;
-    // TODO: piping multiple files in doesn't work. probably a fish problem?
     std::cin >> arrangement;
     arrangement.read_guards(std::cin);
-
+    arrangement.get_reflex_vertices();
+    arrangement.optimise();
     // std::cout << arrangement;
     // arrangement.print_guards(std::cout);
 
-    auto visible_vertices = arrangement.compute_full_visibility();
+    // auto visible_vertices = arrangement.compute_full_visibility();
     // for (auto v : visible_vertices)
         // std::cout << v << std::endl;
-    std::cout << arrangement.is_completely_visible(visible_vertices) << std::endl;
+    // std::cout << arrangement.is_completely_visible(visible_vertices) << std::endl;
 
     return 0;
 }
