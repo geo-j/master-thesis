97da65d7ced22238ac867b60e84e87610de73b6c
geo-j <g.juglan@tutanota.com>
2022-03-24 11:38:22 +0100

    make visibility region class scripts

--- code/visualisations/draw_visibility.py
+++ code/visualisations/draw_visibility.py
@@ -1,55 +1,7 @@
-from skgeom.draw import draw
-from skgeom import Segment2, Point2, arrangement, RotationalSweepVisibility, TriangularExpansionVisibility
+from drawing import Drawing
 import matplotlib.pyplot as plt
-from numpy import random
 
-
-arrangement = arrangement.Arrangement()
-vs = TriangularExpansionVisibility(arrangement)
-guards = []
-
-"""The format of the input file is:
-    E                     * number of edges
-    p1.x p1.y p2.x p2.y   * edge with endpoints coordinates separated by spaces p1(x, y)p2(x, y)
-    p3.x p3.y p4.x p4.y
-    ...
-    IV                    * number of isolated vertices (intended to be guards)
-    q1.x q1.y             * isolated vertex with coordinates q1(x, y) separated by spaces
-    q2.x q2.y
-    ...
-"""
-E = int(input())
-
-for e in range(E):
-    segment = list(map(int, input().split()))
-    p1 = Point2(*segment[:2])
-    p2 = Point2(*segment[2:])
-
-    arrangement.insert(Segment2(p1, p2))
-
-IV = int(input())
-
-for iv in range(IV):
-    vertex = list(map(float, input().split()))
-    q = Point2(*vertex)
-    guards.append(q)
-
-# draw the guards and their visibility regions
-for guard in guards:
-    face = arrangement.find(guard)
-    vx = vs.compute_visibility(guard, face)
-
-    # use a random colour for each guard
-    color = random.rand(3,)
-    for v in vx.halfedges:
-        draw(v.curve(), point = guard, color=color, visible_point=False, fill = True)
-    
-for guard in guards:
-    draw(guard, color='magenta')
-
-# draw the polygon's boundaries
-for he in arrangement.halfedges:
-    draw(he.curve(), visible_point=True)
-
+drawing = Drawing()
+drawing.read_all_input()
+drawing.draw_all()
 plt.show()
-
---
+++ code/visualisations/drawing.py
@@ -0,0 +1,73 @@
+from skgeom.draw import draw
+from skgeom import Segment2, Point2, arrangement, RotationalSweepVisibility, TriangularExpansionVisibility
+import matplotlib.pyplot as plt
+from numpy import random
+
+class Drawing(object):
+    def __init__(self) -> None:
+        self.arrangement = arrangement.Arrangement()
+        self.vs = TriangularExpansionVisibility(self.arrangement)
+        self.guards = []
+    
+    ###########
+    #  Input #
+    ###########
+    """The format of the input file is:
+        E                     * number of edges
+        p1.x p1.y p2.x p2.y   * edge with endpoints coordinates separated by spaces p1(x, y)p2(x, y)
+        p3.x p3.y p4.x p4.y
+        ...
+        IV                    * number of isolated vertices (intended to be guards)
+        q1.x q1.y             * isolated vertex with coordinates q1(x, y) separated by spaces
+        q2.x q2.y
+        ...
+    """
+    def read_input_arrangement(self) -> None:
+        E = int(input())
+
+        for _ in range(E):
+            segment = list(map(int, input().split()))
+            p1 = Point2(*segment[:2])
+            p2 = Point2(*segment[2:])
+
+            self.arrangement.insert(Segment2(p1, p2))
+
+    def read_input_guards(self) -> None:
+        IV = int(input())
+
+        for _ in range(IV):
+            vertex = list(map(float, input().split()))
+            q = Point2(*vertex)
+            self.guards.append(q)
+    
+    def read_all_input(self) -> None:
+        self.read_input_arrangement()
+        self.read_input_guards()
+    
+    ###########
+    #  Output #
+    ###########
+    def draw_visibility_regions(self) -> None:
+        # draw the guards and their visibility regions
+        for guard in self.guards:
+            face = self.arrangement.find(guard)
+            vx = self.vs.compute_visibility(guard, face)
+
+            # use a random colour for each guard
+            color = random.rand(3, )
+            for v in vx.halfedges:
+                draw(v.curve(), point = guard, color = color, visible_point = False, fill = True)
+    
+    def draw_guards(self) -> None:
+        for guard in self.guards:
+            draw(guard, color='magenta')
+
+    def draw_arrangement(self) -> None:
+        # draw the polygon's boundaries
+        for he in self.arrangement.halfedges:
+            draw(he.curve(), visible_point=True)
+
+    def draw_all(self) -> None:
+        self.draw_visibility_regions()
+        self.draw_guards()
+        self.draw_arrangement()
