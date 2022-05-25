694c2f20c157fc4f5ddd19522c8600e2c97cdcc9
geo-j <g.juglan@tutanota.com>
2022-05-25 16:29:31 +0200

    move compute area to utils

--- code/utils.hpp
+++ code/utils.hpp
@@ -29,12 +29,23 @@ void push_back_unique(std::vector<type> &v, type element) {
         v.push_back(element);
 }
 
+/* compute_area function
+* :in param Arrangement_2 arrangement:	the arrangement whose area needs to be computed
+* :return double:						the area of the arrangement
+*
+* This function computes the area of an arrangement without holes. It first converts it to a simple polygon, then uses the built-in CGAL function to compute its area and return it.
+*/
+double compute_area(Arrangement_2 &arrangement) {
+	auto visibility_polygon = arrangement_to_polygon(arrangement);
+	return -CGAL::to_double(visibility_polygon.area());
+}
+
 /* distance function
 * :in param Point_2 p1:		source point
 * :in param Point_2 p2:		destination point
 * :return double:			square distance between the two input points
 *
-* This function compute the square distance p1^2 + p2^2 between two points p1 and p2.
+* This function computes the square distance p1^2 + p2^2 between two points p1 and p2.
 */
 double distance(Point_2 p1, Point_2 p2) {
 	return CGAL::to_double((p1.x() - p2.x()) * (p1.x() - p2.x()) + (p1.y() - p2.y()) * (p1.y() - p2.y()));
