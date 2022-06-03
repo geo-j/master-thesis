38c67f195a69807502925ec8b56f03e3ef2030eb
geo-j <geo.potato@tutanota.com>
2022-06-03 14:52:16 +0200

    read new log file

--- code/visualisations/drawing.py
+++ code/visualisations/drawing.py
@@ -14,10 +14,12 @@ class Drawing(object):
         self.guards = []
         self.halfedges = []
         # dict for each guard's x and y coord
+        self.n_iterations = None
         self.xs = defaultdict(list)
         self.ys = defaultdict(list)
         self.dfs_x = defaultdict(lambda: defaultdict(list))
         self.dfs_y = defaultdict(lambda: defaultdict(list))
+        self.local_areas = defaultdict(lambda: defaultdict(list))
         self.areas = []
 
     
@@ -55,22 +57,31 @@ class Drawing(object):
             self.guards.append(q)
     
     def read_guards_paths(self) -> None:
+        iteration = None
+
         for line in stdin:
-            if line.startswith('g'):
+            if line.startswith('i='):   # get current iteration
+                iteration = int(line[2:].strip())
+            elif line.startswith('g'):  # get current guard coords
                 i = int(line[1])    # get the guard index
                 x, y = map(float, line[3:].strip().split())     # get the coords after removing the guard info
 
                 self.xs[f'g{i}'].append(x)
                 self.ys[f'g{i}'].append(y)
-            elif line.startswith('D'):
+            elif line.startswith('D'):  # get current guard gradient info
                 i = int(line[2])    # get guard index
                 x, y = map(float, line[4:].strip().split())     # get the coords after removing the guard info
-                # print(i, self.xs[f'g{i}'])
-                self.dfs_x[f'g{i}'][(len(self.xs[f'g{i}']) - 1)].append(x)
-                self.dfs_y[f'g{i}'][(len(self.xs[f'g{i}']) - 1)].append(y)
-            else:
+                self.dfs_x[f'g{i}'][iteration].append(x)
+                self.dfs_y[f'g{i}'][iteration].append(y)
+            elif line.startswith('area='):  # get current total seen area
                 area = float(line[5:].strip())
                 self.areas.append(area)
+            elif line.startswith('area'):   # get current guard's area
+                i = int(line[4])
+                area = float(line[6:].strip())
+                self.local_areas[f'g{i}'][iteration].append(area)
+        
+        self.n_iterations = iteration
 
     
     def read_all_input(self) -> None:
@@ -153,19 +164,21 @@ class Drawing(object):
 
 
     def draw_guard_visibility_dfs(self) -> None:
-        for pos in range(0, len(self.xs['g0'])):
+        self.draw_arrangement()
+        for pos in range(self.n_iterations + 1):
             self.draw_visibility_regions(pos)
             self.draw_guards_dfs(pos)
-            plt.title(f'Gradient Computation for Iteration #{int(pos)}')
+            plt.title(f'Gradient Computation for Iteration #{pos}')
 
             path = 'results/'
             date = time.strftime("%Y-%m-%d")
             if not os.path.exists(path + date):
                 os.makedirs(path + date)
-            plt.savefig(f'{path + date}/{time.strftime("%H%M")}_pos{int(pos)}.png', format = 'png')
+            plt.savefig(f'{path + date}/{time.strftime("%H%M")}_pos{pos}.png', format = 'png')
             plt.clf()
             # plt.show()
             self.draw_arrangement()
+        plt.clf()
 
     def plot_area_time(self) -> None:
         plt.plot(self.areas)
