8f8af46f6c62ffdcca8aa3e2028f9f80a7fe30a7
geo-j <g.juglan@tutanota.com>
2022-06-08 14:07:11 +0200

    remove pos

--- code/visualisations/draw_guard_path.py
+++ code/visualisations/draw_guard_path.py
@@ -3,17 +3,15 @@ from sys import stdin
 import numpy as np
 from drawing import Drawing
 
-pos = -1
-
 drawing = Drawing()
 drawing.read_input_arrangement()
-# drawing.draw_arrangement()
+drawing.draw_arrangement()
 drawing.read_guards_paths()
 # drawing.draw_guards()
 # drawing.draw_guards_paths()
 # drawing.draw_visibility_regions(pos)
 # drawing.draw_guards_dfs(pos)
-# drawing.draw_guard_visibility_dfs()
+drawing.draw_guard_visibility_dfs()
 drawing.plot_area_time()
 
 # plt.axis('off')
