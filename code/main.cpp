faf6f6315fa771a022eead71ca3ab57282cf4dad
geo-j <geo.potato@tutanota.com>
2022-06-30 11:23:33 +0200

    rename refactorings

--- code/main.cpp
+++ code/main.cpp
@@ -12,7 +12,7 @@ int main() {
     // arrangement.print_guards(std::cout);
     arrangement.optimise();
 
-    // auto visible_vertices = arrangement.compute_full_visibility();
+    // auto visible_vertices = arrangement.full_visibility();
     // for (auto v : visible_vertices)
         // std::cout << v << std::endl;
     // std::cout << arrangement.is_completely_visible(visible_vertices) << std::endl;
